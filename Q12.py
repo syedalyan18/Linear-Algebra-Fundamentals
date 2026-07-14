b79f55518581d6e079cf9aca7ba1162503ef6902
0648-Alyan <k250648@nu.edu.pk>
2026-07-14 23:35:32 +0500

    INITIAL COMMIT

---
+++ Q12.py
@@ -0,0 +1,15 @@
+from statistics import mode
+
+data=[10,60,20,30,20]
+mean=sum(data)/len(data)
+print("Mean : ",mean)
+
+sorted_data=sorted(data)
+median = sorted_data[len(data)//2] if len(data) % 2!= 0 else\
+ (sorted_data[len(data)//2 - 1] + sorted_data[len(data)//2]) / 2
+
+print("Median : ",median)
+
+mode=mode(data)
+print("Mode : ",mode)
+
