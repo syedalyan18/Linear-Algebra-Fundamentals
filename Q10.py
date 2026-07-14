e5b2008746c1395cab398567ac129dcd8623c2d6
0648-Alyan <k250648@nu.edu.pk>
2026-07-14 21:51:09 +0500

    INITIAL COMMIT

--- Q10.py
+++ Q10.py
@@ -3,9 +3,17 @@ import matplotlib.pyplot as plt
 
 from scipy.stats import binom
 
-n,p=10,0.5
-x=np.arange(0,n+1)
-y=binom.pmf(x,n,p)
-plt.bar(x,y, color="green")
-plt.title("Binomial Distribution")
+# n,p=10,0.5
+# x=np.arange(0,n+1)
+# y=binom.pmf(x,n,p)
+# plt.bar(x,y, color="green")
+# plt.title("Binomial Distribution")
+# plt.show()
+
+from scipy.stats import poisson
+lam=3
+x=np.arange(0,10)
+y=poisson.pmf(x,lam)
+plt.bar(x,y,color="pink")
+plt.title("Poisson Distribution")
 plt.show()
