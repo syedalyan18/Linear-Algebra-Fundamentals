eb326c98d471722c9fe79fe39474195eca4ed0d9
0648-Alyan <k250648@nu.edu.pk>
2026-07-14 23:52:59 +0500

    INITIAL COMMIT

--- Q12.py
+++ Q12.py
@@ -13,3 +13,8 @@ print("Median : ",median)
 mode=mode(data)
 print("Mode : ",mode)
 
+variance=sum((x - mean)**2 for x in data ) / len(data) 
+print("Variance : ",variance)
+
+std_dev=variance ** 0.5
+print("Standard Deviation : ",std_dev)
