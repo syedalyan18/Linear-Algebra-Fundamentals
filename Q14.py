161d38f5d2a141e0973329a9b6c56f63a2adc7d8
0648-Alyan <k250648@nu.edu.pk>
2026-07-14 23:55:39 +0500

    INITIAL COMMIT

---
+++ Q14.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+data=[10,20,30,40,50]
+
+mean=np.mean(data)
+variance=np.var(data)
+std_dev=np.std(data)
+
+print("Mean : ",mean)
+print("Variance : ",variance)
+print("Std Deviation : ",std_dev)
